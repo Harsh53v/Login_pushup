ba94ef9ccb952da388a26429bde597ccb3dbcf27
Harsh <harsh53v@gmail.com>
2016-11-10 19:17:11 +0530

    Rigester activity push

--- app/src/main/java/com/example/harsh53v/login_pushup/Register.java
+++ app/src/main/java/com/example/harsh53v/login_pushup/Register.java
@@ -3,12 +3,11 @@ package com.example.harsh53v.login_pushup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_register);
     }
 }
