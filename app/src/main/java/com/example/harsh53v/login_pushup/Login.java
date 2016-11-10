cf9c7b41ff7401deb3db304216985e3aaccd1099
Harsh <harsh53v@gmail.com>
2016-11-10 18:45:45 +0530

    Login Initial commit

---
+++ app/src/main/java/com/example/harsh53v/login_pushup/Login.java
@@ -0,0 +1,21 @@
+package com.example.harsh53v.login_pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        private void Login(){
+        int i=+;
+
+
+    }
+
+    }
+}
